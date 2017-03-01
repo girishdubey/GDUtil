4399112c36b4e1bb53c2825066dcedb80eb5845f
girishdubey <dubey.girish2@gmail.com>
2017-03-01 23:23:44 +0530

    new version

--- GDUtil.js
+++ GDUtil.js
@@ -15,21 +15,4 @@ directive('onlyDigits', function () {
             });
         }
     };
-}).
-directive('onlyNumber', function () {
-    return {
-        restrict: 'A',
-        require: '?ngModel',
-        link: function (scope, element, attrs, modelCtrl) {
-            modelCtrl.$parsers.push(function (inputValue) {
-                if (inputValue == undefined) return '';
-                var transformedInput = inputValue.replace(/[^0-9]/g, '');
-                if (transformedInput !== inputValue) {
-                    modelCtrl.$setViewValue(transformedInput);
-                    modelCtrl.$render();
-                }
-                return transformedInput;
-            });
-        }
-    };
 });
